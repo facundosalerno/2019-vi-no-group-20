9c4f3b383894729d36906e6882d5d9290ba7dd12
Abdul <abdulganem@gmail.com>
2019-04-19 22:23:39 -0300

    primera aproximación a sugerencia
    por ahora la combinación sugerida es la primera que se cargo.

--- src/main/java/domain/Atuendo.java
+++ src/main/java/domain/Atuendo.java
@@ -4,15 +4,28 @@ import java.util.List;
 
 public class Atuendo {
 
-	Prenda predaSuperior;
-	Prenda predaInferior;
-	Prenda predaCalzado;
+	Prenda prendaSuperior;
+	Prenda prendaInferior;
+	Prenda prendaCalzado;
 	boolean validez;
 
+	
 	public boolean getValidez() {
 	return this.validez;
 		
 	}
+	
+	public Atuendo  (Prenda prendaSuperior,Prenda prendaInferior,Prenda prendaCalzado) {
+		
+		this.prendaSuperior=prendaSuperior;
+		this.prendaInferior=prendaInferior;
+		this.prendaCalzado=prendaCalzado;
+		this.validez=false;
+		this.validarAtuendo(prendaSuperior,prendaInferior, prendaCalzado);
+	}
 
+	public void validarAtuendo (Prenda prendaSuperior,Prenda prendaInferior,Prenda prendaCalzado) {
+		this.validez=true;
+	}
 
 }
--- src/main/java/domain/Guardarropas.java
+++ src/main/java/domain/Guardarropas.java
@@ -1,20 +1,64 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Guardarropas {
 	
-	List<Prenda> predasSuperior;
-	List<Prenda> predasInferior;
-	List<Prenda> predasCalzador;
+	List<Prenda> prendasSuperior;
+	List<Prenda> prendasInferior;
+	List<Prenda> prendasCalzado;
 
+//va a generar atuendos hasta que encuentre uno valido por defecto el atuendo se crea no valido y luego se lo valida.	
 public Atuendo generarSugerencia() {
-	Atuendo atuendo;
-	while atuendo.atuendoValido();{
-	atuendo = combinar(predasSuperior,predasInferior,predasCalzador);
-	}
+	Atuendo sugerencia;
+	do {
 	
-	return atuendo;
+		sugerencia = this.combinar(prendasSuperior,prendasInferior,prendasCalzado);
+		
+	}while (sugerencia.getValidez());
+	
+	return sugerencia;
 	
 }
+public Atuendo combinar(List<Prenda> prendasSuperior,List<Prenda>prendasInferior,List<Prenda>prendasCalzado) {
+	
+	//por ahora agarro el primero de la lista y genero la sugerencia
+	Prenda prendaSuperior = prendasSuperior.stream().findFirst().get();
+	Prenda prendaInferior = prendasSuperior.stream().findFirst().get();
+	Prenda prendaCalzado = prendasSuperior.stream().findFirst().get(); 
+	
+	
+	
+	Atuendo sugerencia= new Atuendo(prendaSuperior,prendaInferior, prendaCalzado);
+	return sugerencia;
+};
+
+public Guardarropas(List<Prenda> prendasSuperior,List<Prenda>prendasInferior,List<Prenda>prendasCalzado) {
+	
+	this.prendasSuperior= prendasSuperior;
+	this.prendasInferior= prendasInferior;	
+	this.prendasCalzado= prendasCalzado;
+	
+	
+}
+
+public void agregarPrendaSuperior(Prenda prendaSuperior) {
+	
+	this.prendasSuperior.add(prendaSuperior);	
+};
+
+public void agregarPrendaInferior(Prenda prendaInferior) {
+	
+	this.prendasSuperior.add(prendaInferior);	
+};
+
+public void agregarPrendaCalzado(Prenda prendaCalzado) {
+	
+	this.prendasSuperior.add(prendaCalzado);	
+};
+
+
+
+
 }
