d58b4e6f81b3e65bf9860c254ee08f16cb8181a1
fdalmaup <dalmaufacundo@gmail.com>
2019-04-22 16:33:11 -0300

    Indento clases Atuendo y Guardarropas

--- src/main/java/domain/Atuendo.java
+++ src/main/java/domain/Atuendo.java
@@ -4,28 +4,28 @@ import java.util.List;
 
 public class Atuendo {
 
-	Prenda prendaSuperior;
-	Prenda prendaInferior;
-	Prenda prendaCalzado;
-	boolean validez;
+    Prenda prendaSuperior;
+    Prenda prendaInferior;
+    Prenda prendaCalzado;
+    boolean validez;
 
-	
-	public boolean getValidez() {
-	return this.validez;
-		
-	}
-	
-	public Atuendo  (Prenda prendaSuperior,Prenda prendaInferior,Prenda prendaCalzado) {
-		
-		this.prendaSuperior=prendaSuperior;
-		this.prendaInferior=prendaInferior;
-		this.prendaCalzado=prendaCalzado;
-		this.validez=false;
-		this.validarAtuendo(prendaSuperior,prendaInferior, prendaCalzado);
-	}
 
-	public void validarAtuendo (Prenda prendaSuperior,Prenda prendaInferior,Prenda prendaCalzado) {
-		this.validez=true;
-	}
+    public boolean getValidez() {
+        return this.validez;
+
+    }
+
+    public Atuendo(Prenda prendaSuperior, Prenda prendaInferior, Prenda prendaCalzado) {
+
+        this.prendaSuperior = prendaSuperior;
+        this.prendaInferior = prendaInferior;
+        this.prendaCalzado = prendaCalzado;
+        this.validez = false;
+        this.validarAtuendo(prendaSuperior, prendaInferior, prendaCalzado);
+    }
+
+    public void validarAtuendo(Prenda prendaSuperior, Prenda prendaInferior, Prenda prendaCalzado) {
+        this.validez = true;
+    }
 
 }
--- src/main/java/domain/Guardarropas.java
+++ src/main/java/domain/Guardarropas.java
@@ -4,61 +4,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Guardarropas {
-	
-	List<Prenda> prendasSuperior;
-	List<Prenda> prendasInferior;
-	List<Prenda> prendasCalzado;
 
-//va a generar atuendos hasta que encuentre uno valido por defecto el atuendo se crea no valido y luego se lo valida.	
-public Atuendo generarSugerencia() {
-	Atuendo sugerencia;
-	do {
-	
-		sugerencia = this.combinar(prendasSuperior,prendasInferior,prendasCalzado);
-		
-	}while (sugerencia.getValidez());
-	
-	return sugerencia;
-	
-}
-public Atuendo combinar(List<Prenda> prendasSuperior,List<Prenda>prendasInferior,List<Prenda>prendasCalzado) {
-	
-	//por ahora agarro el primero de la lista y genero la sugerencia
-	Prenda prendaSuperior = prendasSuperior.stream().findFirst().get();
-	Prenda prendaInferior = prendasSuperior.stream().findFirst().get();
-	Prenda prendaCalzado = prendasSuperior.stream().findFirst().get(); 
-	
-	
-	
-	Atuendo sugerencia= new Atuendo(prendaSuperior,prendaInferior, prendaCalzado);
-	return sugerencia;
-};
+    List<Prenda> prendasSuperior;
+    List<Prenda> prendasInferior;
+    List<Prenda> prendasCalzado;
 
-public Guardarropas(List<Prenda> prendasSuperior,List<Prenda>prendasInferior,List<Prenda>prendasCalzado) {
-	
-	this.prendasSuperior= prendasSuperior;
-	this.prendasInferior= prendasInferior;	
-	this.prendasCalzado= prendasCalzado;
-	
-	
-}
+    //va a generar atuendos hasta que encuentre uno valido por defecto el atuendo se crea no valido y luego se lo valida.
+    public Atuendo generarSugerencia() {
+        Atuendo sugerencia;
+        do {
 
-public void agregarPrendaSuperior(Prenda prendaSuperior) {
-	
-	this.prendasSuperior.add(prendaSuperior);	
-};
+            sugerencia = this.combinar(prendasSuperior, prendasInferior, prendasCalzado);
 
-public void agregarPrendaInferior(Prenda prendaInferior) {
-	
-	this.prendasSuperior.add(prendaInferior);	
-};
+        } while (sugerencia.getValidez());
 
-public void agregarPrendaCalzado(Prenda prendaCalzado) {
-	
-	this.prendasSuperior.add(prendaCalzado);	
-};
+        return sugerencia;
+
+    }
+
+    public Atuendo combinar(List<Prenda> prendasSuperior, List<Prenda> prendasInferior, List<Prenda> prendasCalzado) {
+
+        //por ahora agarro el primero de la lista y genero la sugerencia
+        Prenda prendaSuperior = prendasSuperior.stream().findFirst().get();
+        Prenda prendaInferior = prendasSuperior.stream().findFirst().get();
+        Prenda prendaCalzado = prendasSuperior.stream().findFirst().get();
 
 
+        Atuendo sugerencia = new Atuendo(prendaSuperior, prendaInferior, prendaCalzado);
+        return sugerencia;
+    }
+
+    public Guardarropas(List<Prenda> prendasSuperior, List<Prenda> prendasInferior, List<Prenda> prendasCalzado) {
+
+        this.prendasSuperior = prendasSuperior;
+        this.prendasInferior = prendasInferior;
+        this.prendasCalzado = prendasCalzado;
 
 
+    }
+
+    public void agregarPrendaSuperior(Prenda prendaSuperior) {
+
+        this.prendasSuperior.add(prendaSuperior);
+    }
+
+    public void agregarPrendaInferior(Prenda prendaInferior) {
+
+        this.prendasSuperior.add(prendaInferior);
+    }
+
+    public void agregarPrendaCalzado(Prenda prendaCalzado) {
+
+        this.prendasSuperior.add(prendaCalzado);
+    }
 }
