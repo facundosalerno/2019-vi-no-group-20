b02fb2662129804c8bf66d41982ce1eeb1a8cd11
Abdul <abdulganem@gmail.com>
2019-04-22 22:38:28 -0300

    Se actualiza el metodo combinar
    Ahora se llama combinarPrendas y devuelve una lista de atuendos combinando la listas de prendas superiores, inferiores y calzados

--- src/main/java/domain/Guardarropas.java
+++ src/main/java/domain/Guardarropas.java
@@ -2,6 +2,11 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 
 public class Guardarropas {
 
@@ -11,7 +16,8 @@ public class Guardarropas {
 
     //va a generar atuendos hasta que encuentre uno valido por defecto el atuendo se crea no valido y luego se lo valida.
     public Atuendo generarSugerencia() {
-        Atuendo sugerencia;
+       /*
+    	Atuendo sugerencia;
         do {
 
             sugerencia = this.combinar(prendasSuperior, prendasInferior, prendasCalzado);
@@ -19,19 +25,23 @@ public class Guardarropas {
         } while (sugerencia.getValidez());
 
         return sugerencia;
-
+        */
+    	
     }
 
-    public Atuendo combinar(List<Prenda> prendasSuperior, List<Prenda> prendasInferior, List<Prenda> prendasCalzado) {
+    public List<Atuendo> combinarPrendas(List<Prenda> prendasSuperior, List<Prenda> prendasInferior, List<Prenda> prendasCalzado) {
 
         //por ahora agarro el primero de la lista y genero la sugerencia
-        Prenda prendaSuperior = prendasSuperior.stream().findFirst().get();
-        Prenda prendaInferior = prendasSuperior.stream().findFirst().get();
-        Prenda prendaCalzado = prendasSuperior.stream().findFirst().get();
+       // Prenda prendaSuperior = prendasSuperior.stream().findFirst().get();
+        //Prenda prendaInferior = prendasSuperior.stream().findFirst().get();
+        //Prenda prendaCalzado = prendasSuperior.stream().findFirst().get();
 
-
-        Atuendo sugerencia = new Atuendo(prendaSuperior, prendaInferior, prendaCalzado);
-        return sugerencia;
+       // Atuendo sugerencia = new Atuendo(prendaSuperior, prendaInferior, prendaCalzado);
+       
+    	return 	Sets.cartesianProduct(ImmutableList.of(ImmutableSet.copyOf(this.prendasSuperior), ImmutableSet.copyOf(this.prendasInferior), ImmutableSet.copyOf(this.prendasCalzado)))
+        		.stream()
+        		.map(list -> new Atuendo(list.get(0), list.get(1), list.get(2)))
+        	     .collect(Collectors.toList());
     }
 
     public Guardarropas(List<Prenda> prendasSuperior, List<Prenda> prendasInferior, List<Prenda> prendasCalzado) {
