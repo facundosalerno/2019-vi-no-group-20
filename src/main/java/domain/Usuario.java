cb2171aa8d34ec962411f17d3ef93ef00075d46b
Facu Salerno <facusalerno@MacBook-Air-de-Facu.local>
2019-04-24 15:34:15 -0300

    Implementacion de multiples usuarios

---
+++ src/main/java/domain/Usuario.java
@@ -0,0 +1,21 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario {
+    private List<Guardarropas> guardarropas;
+    //TODO: FALTA DECIDIR SI EL USUARIO LLEVA LISTAS DE PRENDAS (PARTE SUPERIOR, INFERIOR, ETC) O SOLO UN ATRIBUTO POR CADA PRENDA SIMULANDO QUE ESTA VESTIDO
+
+    public Usuario(List<Guardarropas> guardarropas){
+        this.guardarropas = guardarropas;
+    }
+
+    public List<Atuendo> obtenerSugerencias(){
+        //TODO: RESOLVER DE UNA MEJOR FORMA
+        List<Atuendo> aux = new ArrayList<Atuendo>();
+        guardarropas.stream()
+                .forEach(guardarropa -> aux.addAll(guardarropa.sugerir()));
+        return aux;
+    }
+}
