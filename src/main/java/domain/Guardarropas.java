87ef21be8be65f071011453f4c465f76c5b2a589
carolinacaampos <38320197+carolinacaampos@users.noreply.github.com>
2019-04-27 18:54:33 -0300

    agrego prenda accesorio

--- src/main/java/domain/Guardarropas.java
+++ src/main/java/domain/Guardarropas.java
@@ -5,6 +5,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import exceptions.NoPerteneceALaCategoriaException;
+import exceptions.NoPermiteGuardarropaIncompletoException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,26 +15,29 @@ public class Guardarropas {
     private List<Prenda> prendasSuperiores;
     private List<Prenda> prendasInferiores;
     private List<Prenda> calzados;
+    private List<Prenda> accesorios;
 
-    public Guardarropas(List<Prenda> prendasSuperiores, List<Prenda> prendasInferiores, List<Prenda> calzados) {
+    public Guardarropas(List<Prenda> prendasSuperiores, List<Prenda> prendasInferiores, List<Prenda> calzados, List<Prenda> accesorios) {
         prendasCoincidenConCategoria(prendasSuperiores, Categoria.PARTE_SUPERIOR);
         prendasCoincidenConCategoria(prendasInferiores, Categoria.PARTE_INFERIOR);
         prendasCoincidenConCategoria(calzados, Categoria.CALZADO);
+        prendasCoincidenConCategoria(accesorios, Categoria.ACCESORIOS);
 
         this.prendasSuperiores = prendasSuperiores;
         this.prendasInferiores = prendasInferiores;
         this.calzados = calzados;
+        this.accesorios= accesorios;
     }
 
     public List<Atuendo> sugerirAtuendo(){
-
-        if(this.prendasSuperiores.size()!=0 && this.prendasInferiores.size()!=0 && this.calzados.size()!= 0)
-            {
-                return Sets.cartesianProduct(ImmutableList.of(ImmutableSet.copyOf(prendasSuperiores), ImmutableSet.copyOf(prendasInferiores), ImmutableSet.copyOf(calzados)))
-                        .stream()
-                        .map(list -> new Atuendo(list.get(0), list.get(1), list.get(2)))
-                        .collect(Collectors.toList());
-            }
+        //TODO: hacer mas lindo este if
+        if(this.prendasSuperiores.size()!=0 && this.prendasInferiores.size()!=0 && this.calzados.size()!= 0 && this.accesorios.size()!=0)
+        {
+            return Sets.cartesianProduct(ImmutableList.of(ImmutableSet.copyOf(prendasSuperiores), ImmutableSet.copyOf(prendasInferiores), ImmutableSet.copyOf(accesorios), ImmutableSet.copyOf(calzados)))
+                    .stream()
+                    .map(list -> new Atuendo(list.get(0), list.get(1), list.get(2),list.get(3)))
+                    .collect(Collectors.toList());
+        }
         throw new NoPermiteGuardarropaIncompletoException();
 
     }
