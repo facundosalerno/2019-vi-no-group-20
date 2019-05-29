1b6635953137a9477d5b7f67b6f1a0b1eb41ee55
Abdul <abdulganem@gmail.com>
2019-05-29 13:52:57 -0300

    Se crea la clase TemperaturaOpenWeather

--- src/main/java/clima/OpenWeather.java
+++ src/main/java/clima/OpenWeather.java
@@ -1,20 +1,31 @@
 package clima;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import conexionesUrl.NetClientGet;
 
 public class OpenWeather implements ApiExterna{
     private final String URI = "https://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,%20AR&appid=a4eb7e978f43ec94e5cf3885b5dc3b2c";
-    private OpenWeatherClimaActual climaActual;
+    private TemperaturaOpenWeather climaActual;
     private String jsonData;
 
     public void obtenerClima() {
-        jsonData = NetClientGet.main(URI);
+        jsonData = NetClientGet.main(URI);    
+      
         final Gson gson = new Gson();
-        climaActual = gson.fromJson(jsonData, OpenWeatherClimaActual.class);
+		
+		JsonParser parser = new JsonParser();
+		JsonObject element = (JsonObject)parser.parse(gson.toString());
+
+		JsonElement responseWrapper = element.get("main");
+
+	    final TemperaturaOpenWeather climaActual = gson.fromJson(jsonData, TemperaturaOpenWeather.class);
     }
 
-    public OpenWeatherClimaActual devolverClima() {
+    public TemperaturaOpenWeather devolverClima() {
         return climaActual;
     }
 
---
+++ src/main/java/clima/TemperaturaOpenWeather.java
@@ -0,0 +1,25 @@
+package clima;
+
+public class TemperaturaOpenWeather implements Clima {
+
+	double temp;
+    double pressure;
+    double humidity;
+    double temp_min;
+    double temp_max;
+	
+    public TemperaturaOpenWeather(double temp,double pressure,double humidity,double temp_min,double temp_max) {
+    	this.temp=temp;
+    	this.pressure=pressure;
+    	this.humidity=humidity;
+    	this.temp_min=temp_min;
+    	this.temp_max=temp_max;
+    }
+    
+	@Override
+	public double getTemperature() {
+	
+		return this.temp;
+	}
+
+}
