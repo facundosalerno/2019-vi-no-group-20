f6cffbd5389feb0b02d2bb92bfbd4bacbd46b80e
facundosalerno <facundosalerno1@gmail.com>
2019-06-26 15:25:57 -0300

    los tipos de prenda manejan distinto la temperatura
    Si no importa la temperatura, no se pone en el constructor. Si importa, se agrega un rango

--- src/main/java/domain/prenda/TemperaturaAdecuadaPrenda.java
+++ src/main/java/domain/prenda/TemperaturaAdecuadaPrenda.java
@@ -3,32 +3,30 @@ package domain.prenda;
 import exceptions.TemperaturaInvalidaException;
 
 public class TemperaturaAdecuadaPrenda {
+    private final int rangoMaximoTeorico = 60;
+    private final int getRangoMinimoTeorico = -30;
     private int rangoMaximo;
     private int rangoMinimo;
-    private boolean importaLaTemperatura; //Para los accesorios por ejemplo
 
     public TemperaturaAdecuadaPrenda(int rangoMinimo, int rangoMaximo){
-        if((this.rangoMaximo = rangoMaximo) > 60)
+        if((this.rangoMaximo = rangoMaximo) > rangoMaximoTeorico)
             throw new TemperaturaInvalidaException("Vaya, esa temperatura es muy alta");
-        if((this.rangoMinimo = rangoMinimo) < -30)
+        if((this.rangoMinimo = rangoMinimo) < getRangoMinimoTeorico)
             throw new TemperaturaInvalidaException("Vaya, esa temperatura es muy baja");
         if(rangoMaximo < rangoMinimo)
             throw new TemperaturaInvalidaException("El rango maximo deberia ser mayor que el rango minimo");
-        importaLaTemperatura = true;
     }
 
-    public TemperaturaAdecuadaPrenda(){
-        importaLaTemperatura = false;
-    }
 
     public boolean temperaturaSeEncuentraEnElRango(int temperatura){
-        if(importaLaTemperatura)
             return temperatura > rangoMinimo && temperatura < rangoMaximo;
-        else
-            return true;
     }
 
     public int getRangoMaximo(){
         return rangoMaximo;
+    }
+
+    static int getRangoMaximoTeorico(){
+        return getRangoMaximoTeorico();
     }
 }
--- src/main/java/domain/prenda/TipoDePrenda.java
+++ src/main/java/domain/prenda/TipoDePrenda.java
@@ -15,11 +15,20 @@ public class TipoDePrenda {
         this.temperatura = temperatura;
     }
 
+    public TipoDePrenda(Categoria categoria, List<Material> materiales) {
+        this.categoria = categoria;
+        materialesValidos = materiales;
+    }
+
     public boolean esAptaParaTemperatura(int temperatura){
+        if(this.temperatura == null)
+            return true;
         return this.temperatura.temperaturaSeEncuentraEnElRango(temperatura);
     }
 
     public int temperaturaResistida(){
+        if(temperatura == null)
+            return TemperaturaAdecuadaPrenda.getRangoMaximoTeorico();
         return temperatura.getRangoMaximo();
     }
 
@@ -32,17 +41,17 @@ public class TipoDePrenda {
     }
 
     //Atributos estaticos de prueba, hacer todos los necesarios
-    public static final TipoDePrenda ZAPATO = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.CUERO,Material.GAMUZA), new TemperaturaAdecuadaPrenda());
+    public static final TipoDePrenda ZAPATO = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.CUERO,Material.GAMUZA));
     public static final TipoDePrenda REMERA = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.ALGODON), new TemperaturaAdecuadaPrenda(0, 30));
     public static final TipoDePrenda CAMISA = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.ALGODON,Material.POLIESTER), new TemperaturaAdecuadaPrenda(0, 29));
     public static final TipoDePrenda PANTALON = new TipoDePrenda(Categoria.PARTE_INFERIOR, Arrays.asList(Material.JEAN), new TemperaturaAdecuadaPrenda(0, 30));
     public static final TipoDePrenda SHORT = new TipoDePrenda(Categoria.PARTE_INFERIOR, Arrays.asList(Material.ALGODON,Material.JEAN), new TemperaturaAdecuadaPrenda(23, 35));
     public static final TipoDePrenda BLUSA = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.ALGODON), new TemperaturaAdecuadaPrenda(10, 24));
-    public static final TipoDePrenda ZAPATILLA = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.GAMUZA), new TemperaturaAdecuadaPrenda());
+    public static final TipoDePrenda ZAPATILLA = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.GAMUZA));
     public static final TipoDePrenda POLLERA = new TipoDePrenda(Categoria.PARTE_INFERIOR, Arrays.asList(Material.POLIESTER), new TemperaturaAdecuadaPrenda(15, 30));
     public static final TipoDePrenda OJOTAS = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.GOMA), new TemperaturaAdecuadaPrenda(23, 35));
     public static final TipoDePrenda ALPARGATAS = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.ALGODON), new TemperaturaAdecuadaPrenda(-5, 25));
     public static final TipoDePrenda ZAPATO_DE_TACON = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.CUERO), new TemperaturaAdecuadaPrenda(10, 30));
-    public static final TipoDePrenda PULSERA = new TipoDePrenda(Categoria.ACCESORIOS, Arrays.asList(Material.PLASTICO), new TemperaturaAdecuadaPrenda());
-    public static final TipoDePrenda ANTEOJOS = new TipoDePrenda(Categoria.ACCESORIOS, Arrays.asList(Material.PLASTICO), new TemperaturaAdecuadaPrenda());
+    public static final TipoDePrenda PULSERA = new TipoDePrenda(Categoria.ACCESORIOS, Arrays.asList(Material.PLASTICO));
+    public static final TipoDePrenda ANTEOJOS = new TipoDePrenda(Categoria.ACCESORIOS, Arrays.asList(Material.PLASTICO));
 }
