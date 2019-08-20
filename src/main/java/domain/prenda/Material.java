7c828c667e02c688f418cda455233a9ac550f6f1
fdalmaup <dalmaufacundo@gmail.com>
2019-08-19 22:47:57 -0300

    Capas sin fix de tests

--- src/main/java/domain/prenda/Material.java
+++ src/main/java/domain/prenda/Material.java
@@ -11,5 +11,6 @@ public enum Material{
     JEAN,
     GAMUZA,
     GOMA,
-    PLASTICO
+    PLASTICO,
+    GABARDINA
 }
--- src/main/java/domain/prenda/TipoDePrenda.java
+++ src/main/java/domain/prenda/TipoDePrenda.java
@@ -10,15 +10,17 @@ public class TipoDePrenda {
     private Categoria categoria;
     private List<Material> materialesValidos;
     private TemperaturaMaximaDeUso temperatura;
+    private NivelDeCapa nivelDeCapa;
 
-    public TipoDePrenda(Categoria categoria, List<Material> materiales, TemperaturaMaximaDeUso temperatura) {
+    public TipoDePrenda(Categoria categoria, List<Material> materiales, TemperaturaMaximaDeUso temperatura, NivelDeCapa nivelDeCapa) {
         this.categoria = categoria;
         materialesValidos = materiales;
         this.temperatura = temperatura;
+        this.nivelDeCapa=nivelDeCapa;
     }
 
     public NivelDeCapa getNivelDeCapa(){
-        return null;
+        return this.nivelDeCapa;
     }
 
     public Categoria categoria() {
@@ -30,17 +32,22 @@ public class TipoDePrenda {
     }
 
     //Atributos estaticos de prueba, hacer todos los necesarios
-    public static final TipoDePrenda ZAPATO = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.CUERO, Material.GAMUZA), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda REMERA = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.ALGODON), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda CAMISA = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.ALGODON,Material.POLIESTER), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda PANTALON = new TipoDePrenda(Categoria.PARTE_INFERIOR, Arrays.asList(Material.JEAN), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda SHORT = new TipoDePrenda(Categoria.PARTE_INFERIOR, Arrays.asList(Material.ALGODON,Material.JEAN), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda BLUSA = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.ALGODON), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda ZAPATILLA = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.GAMUZA), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda POLLERA = new TipoDePrenda(Categoria.PARTE_INFERIOR, Arrays.asList(Material.POLIESTER), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda OJOTAS = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.GOMA), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda ALPARGATAS = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.ALGODON), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda ZAPATO_DE_TACON = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.CUERO), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda PULSERA = new TipoDePrenda(Categoria.ACCESORIOS, Arrays.asList(Material.PLASTICO), new TemperaturaMaximaDeUso(30));
-    public static final TipoDePrenda ANTEOJOS = new TipoDePrenda(Categoria.ACCESORIOS, Arrays.asList(Material.PLASTICO), new TemperaturaMaximaDeUso(30));
+    public static final TipoDePrenda ZAPATO = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.CUERO, Material.GAMUZA), new TemperaturaMaximaDeUso(35), NivelDeCapa.MEDIO);
+    public static final TipoDePrenda REMERA = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.ALGODON), new TemperaturaMaximaDeUso(35), NivelDeCapa.LIVIANO);
+    public static final TipoDePrenda CAMISA = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.ALGODON,Material.POLIESTER), new TemperaturaMaximaDeUso(35), NivelDeCapa.LIVIANO);
+    public static final TipoDePrenda PANTALON = new TipoDePrenda(Categoria.PARTE_INFERIOR, Arrays.asList(Material.JEAN), new TemperaturaMaximaDeUso(35), NivelDeCapa.LIVIANO);
+    public static final TipoDePrenda SHORT = new TipoDePrenda(Categoria.PARTE_INFERIOR, Arrays.asList(Material.ALGODON,Material.JEAN), new TemperaturaMaximaDeUso(35), NivelDeCapa.LIVIANO);
+    public static final TipoDePrenda BLUSA = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.ALGODON), new TemperaturaMaximaDeUso(35), NivelDeCapa.LIVIANO);
+    public static final TipoDePrenda ZAPATILLA = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.GAMUZA), new TemperaturaMaximaDeUso(35), NivelDeCapa.MEDIO);
+    public static final TipoDePrenda POLLERA = new TipoDePrenda(Categoria.PARTE_INFERIOR, Arrays.asList(Material.POLIESTER), new TemperaturaMaximaDeUso(35), NivelDeCapa.LIVIANO);
+    public static final TipoDePrenda OJOTAS = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.GOMA), new TemperaturaMaximaDeUso(35), NivelDeCapa.LIVIANO);
+    public static final TipoDePrenda ALPARGATAS = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.ALGODON), new TemperaturaMaximaDeUso(35), NivelDeCapa.LIVIANO);
+    public static final TipoDePrenda ZAPATO_DE_TACON = new TipoDePrenda(Categoria.CALZADO, Arrays.asList(Material.CUERO), new TemperaturaMaximaDeUso(35), NivelDeCapa.MEDIO);
+    public static final TipoDePrenda PULSERA = new TipoDePrenda(Categoria.ACCESORIOS, Arrays.asList(Material.PLASTICO), new TemperaturaMaximaDeUso(35), NivelDeCapa.LIVIANO);
+    public static final TipoDePrenda ANTEOJOS = new TipoDePrenda(Categoria.ACCESORIOS, Arrays.asList(Material.PLASTICO), new TemperaturaMaximaDeUso(35), NivelDeCapa.LIVIANO);
+    public static final TipoDePrenda BUSO = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.ALGODON), new TemperaturaMaximaDeUso(20), NivelDeCapa.MEDIO);
+    public static final TipoDePrenda SWEATER = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.LINO), new TemperaturaMaximaDeUso(23), NivelDeCapa.MEDIO);
+    public static final TipoDePrenda CAMPERA = new TipoDePrenda(Categoria.PARTE_SUPERIOR, Arrays.asList(Material.GABARDINA), new TemperaturaMaximaDeUso(15), NivelDeCapa.ELEVADO);
+
+
 }
