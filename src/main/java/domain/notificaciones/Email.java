9522408e448622bdfb7567f7ea2521e2d626c28b
facundosalerno <facundosalerno1@gmail.com>
2019-09-20 00:41:49 -0300

    persistir medios

--- src/main/java/domain/notificaciones/Email.java
+++ src/main/java/domain/notificaciones/Email.java
@@ -1,6 +1,21 @@
 package domain.notificaciones;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value="Email")
 public class Email extends MedioDeNotificacion {
+    private String email;
+
+    public Email(String email){
+        this.email = email;
+    }
+
+    public Email(){
+
+    }
+
     @Override
     public void lanzarNotificacion() {
 
--- src/main/java/domain/notificaciones/SMS.java
+++ src/main/java/domain/notificaciones/SMS.java
@@ -1,6 +1,21 @@
 package domain.notificaciones;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value="SMS")
 public class SMS extends MedioDeNotificacion {
+    private int numero;
+
+    public SMS(int numero){
+        this.numero = numero;
+    }
+
+    public SMS(){
+
+    }
+
     @Override
     public void lanzarNotificacion() {
 
