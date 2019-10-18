ec1608cd1fed9b58306428f5f63b5e01c8f1f6b2
facundosalerno <facundosalerno1@gmail.com>
2019-10-18 17:11:27 -0300

    retoques login

--- src/main/java/controllers/ControllerSesion.java
+++ src/main/java/controllers/ControllerSesion.java
@@ -16,21 +16,25 @@ public class ControllerSesion {
 
     // .../guardarropas/prendas
 
+    private String mensajeSesion = "Nunca compartas tu contraseña con nadie.";
+
     public ModelAndView mostrarLogin(Request req, Response res) {
-        return new ModelAndView(null,"login.hbs");  //Indica con que se va a renderizar el template (.hbs)
+        return new ModelAndView(new ControllerSesion(),"login.hbs");  //Indica con que se va a renderizar el template (.hbs)
     }
 
     public ModelAndView crear(Request req, Response res){
         RepositorioUsuarios.admin.setPassword("12345"); //para test. la pass se hashea en el set
-        Usuario usuario = null;
+        Usuario usuario;
 
         try{
             usuario = RepositorioUsuarios.getInstance().buscarUsuario(req.queryParams("var_username"));
             usuario.validarContraseña(req.queryParams("var_password"));
         }catch(UsuarioInexistente e){
-            return null;
+            mensajeSesion = "Usuario inexistente.";
+            return new ModelAndView(this, "login.hbs");
         }catch (ContraseñaInvalidaException e){
-            return null;
+            mensajeSesion = "Contraseña invalida.";
+            return new ModelAndView(this, "login.hbs");
         }
 
         res.cookie("cookie_nombre", usuario.getNombre());
@@ -39,4 +43,7 @@ public class ControllerSesion {
         return new ModelAndView(usuario, "perfil.hbs");
     }
 
+    public String getMensajeSesion(){
+        return mensajeSesion;
+    }
 }
