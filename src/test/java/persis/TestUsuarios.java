087f4f51cf4e57fd047a1eab2ed40994f8d46c69
fdalmaup <dalmaufacundo@gmail.com>
2019-11-24 23:08:34 -0300

    Test persistencia usuario fix

--- src/test/java/persis/TestUsuarios.java
+++ src/test/java/persis/TestUsuarios.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,6 +23,7 @@ public class TestUsuarios extends AbstractPersistenceTest implements WithGlobalE
 
     private GuardarropasPremium guardarropasCasual;
     private GuardarropasLimitado guardarropasLimitado;
+    private GuardarropasPremium guardarropasCopado;
     private Prenda zapatos;
     private Prenda remera;
     private Prenda camisa;
@@ -36,19 +38,20 @@ public class TestUsuarios extends AbstractPersistenceTest implements WithGlobalE
         Color azul = new Color(0, 0, 255);
         Color blanco = new Color(255, 255, 255);
 
-        zapatos = armarUnaPrenda(TipoDePrenda.ZAPATO, Material.CUERO, rojo, azul, Trama.GASTADO);
-        remera = armarUnaPrenda(TipoDePrenda.REMERA, Material.ALGODON, azul, rojo, Trama.CUADROS);
-        pantalon = armarUnaPrenda(TipoDePrenda.PANTALON, Material.JEAN, verde, rojo, Trama.RAYADA);
-        anteojos= armarUnaPrenda(TipoDePrenda.ANTEOJOS, Material.PLASTICO, verde, rojo, Trama.LISA);
-        camisa = armarUnaPrenda(TipoDePrenda.CAMISA, Material.ALGODON, blanco, rojo, Trama.LISA);
+        zapatos = armarUnaPrenda("zapatos",TipoDePrenda.ZAPATO, Material.CUERO, rojo, azul, Trama.GASTADO);
+        remera = armarUnaPrenda("remera",TipoDePrenda.REMERA, Material.ALGODON, azul, rojo, Trama.CUADROS);
+        pantalon = armarUnaPrenda("pantalon",TipoDePrenda.PANTALON, Material.JEAN, verde, rojo, Trama.RAYADA);
+        anteojos= armarUnaPrenda("anteojos",TipoDePrenda.ANTEOJOS, Material.PLASTICO, verde, rojo, Trama.LISA);
+        camisa = armarUnaPrenda("camisa",TipoDePrenda.CAMISA, Material.ALGODON, blanco, rojo, Trama.LISA);
 
 
         fechaCumpleWilly = LocalDateTime.of(2020,06,20,20,30);
 
     }
 
-    public Prenda armarUnaPrenda(TipoDePrenda tipoDePrenda, Material material, Color colorPrimario, Color colorSecundario, Trama trama){
+    public Prenda armarUnaPrenda(String nombre,TipoDePrenda tipoDePrenda, Material material, Color colorPrimario, Color colorSecundario, Trama trama){
         BorradorPrenda borradorPrenda = new BorradorPrenda();
+        borradorPrenda.definirNombre(nombre);
         borradorPrenda.definirTipo(tipoDePrenda);
         borradorPrenda.definirMaterial(material);
         borradorPrenda.definirColorPrimario(colorPrimario);
@@ -75,5 +78,46 @@ public class TestUsuarios extends AbstractPersistenceTest implements WithGlobalE
         entityManager().persist(facundo);
 
         entityManager().find(Usuario.class,facundo.getId());
+    }
+
+    @Test
+    public void seRecuperaGuardarropasDeUsuario() throws Exception {
+
+        facundo = new Usuario("Facundo Salerno",new ArrayList<Guardarropas>(Arrays.asList(new GuardarropasPremium("guardarropas casual", Arrays.asList(remera, camisa), Arrays.asList(pantalon), Arrays.asList(zapatos), Arrays.asList(anteojos)))), TipoDeUsuario.PREMIUM);
+        guardarropasCopado = new GuardarropasPremium("guardarropas copado", new ArrayList<Prenda>(Arrays.asList(camisa, remera)), new ArrayList<Prenda>(Arrays.asList(pantalon)), new ArrayList<Prenda>(Arrays.asList(zapatos)), new ArrayList<Prenda>(Arrays.asList(anteojos)));
+
+        facundo.agregarGuardarropas(guardarropasCopado);
+
+        entityManager().persist(facundo);
+
+        Usuario hermanoGemeloDeFacundoQueSeCopiaAFacundo = entityManager()
+                .createQuery("from Usuario where id LIKE :IdUsuario", Usuario.class)
+                .setParameter("IdUsuario", facundo.getId())
+                .getResultList()
+                .get(0);
+
+
+        Assert.isTrue(hermanoGemeloDeFacundoQueSeCopiaAFacundo.getGuardarropas(1).getNombre().equals("guardarropas copado"));
+
+
+    }
+
+    @Test
+    public void seRecuperaEventoDeUsuario() throws Exception {
+
+        facundo = new Usuario("Facundo Salerno",new ArrayList<Guardarropas>(Arrays.asList(new GuardarropasPremium("guardarropas casual", Arrays.asList(remera, camisa), Arrays.asList(pantalon), Arrays.asList(zapatos), Arrays.asList(anteojos)))), TipoDeUsuario.PREMIUM);
+        facundo.crearEvento("Cumpleaños de juan", fechaCumpleWilly, FrecuenciaEvento.ANUAL,"Casa de Juan");
+
+        entityManager().persist(facundo);
+
+        Usuario hermanoGemeloDeFacundoQueSeCopiaAFacundo = entityManager()
+                .createQuery("from Usuario where id LIKE :IdUsuario", Usuario.class)
+                .setParameter("IdUsuario", facundo.getId())
+                .getResultList()
+                .get(0);
+
+        System.out.println("Nombre usuario: " +hermanoGemeloDeFacundoQueSeCopiaAFacundo.getNombre());
+
+        Assert.isTrue(hermanoGemeloDeFacundoQueSeCopiaAFacundo.getEventos().get(0).getNombre().equals("Cumpleaños de juan"));
     }
 }
